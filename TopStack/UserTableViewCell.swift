74f81381a9ef1695427bdfaed815e1702effb6b7
Mike Gilroy <mjgilroy@me.com>
2017-07-31 11:04:47 +0100

    Add initial VC, tableview and tableviewcell

---
+++ TopStack/UserTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  UserTableViewCell.swift
+//  TopStack
+//
+//  Created by Mike Gilroy on 31/07/2017.
+//  Copyright © 2017 Mike Gilroy. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+	@IBOutlet weak var userImageView: UIImageView!
+	@IBOutlet weak var userNameLabel: UILabel!
+	@IBOutlet weak var userReputationLabel: UILabel!
+	
+	
+	
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
---
+++ UsersListViewController.swift
@@ -0,0 +1,33 @@
+//
+//  UsersListViewController.swift
+//  TopStack
+//
+//  Created by Mike Gilroy on 31/07/2017.
+//  Copyright © 2017 Mike Gilroy. All rights reserved.
+//
+
+import UIKit
+
+class UsersListViewController: UIViewController {
+
+	// MARK: - Outlets
+	
+	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var reloadButton: UIBarButtonItem!
+	
+	
+	// MARK: - Actions
+	
+	@IBAction func reloadTapped(_ sender: Any) {
+		
+	}
+	
+	
+	// MARK: - VC Life Cycle
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+	
+
+}
