3f5e5dfeab0118c4e23499820359e632c6a29e04
Mike Gilroy <mjgilroy@me.com>
2017-07-31 14:59:50 +0100

    Add StackUserResult

---
+++ TopStack/StackUsersResult.swift
@@ -0,0 +1,27 @@
+//
+//  StackUsersResult.swift
+//  TopStack
+//
+//  Created by Mike Gilroy on 31/07/2017.
+//  Copyright © 2017 Mike Gilroy. All rights reserved.
+//
+
+import Foundation
+
+protocol UsersResult: JSONInitialisable {
+	var items: [User] { get}
+}
+
+struct StackUsersResult: UsersResult {
+
+	private let kItems = "items"
+	
+	let items: [User]
+	
+	init?(json: [String: Any]) {
+		guard let items = json[kItems] as? [[String: Any]] else {
+			return nil
+		}
+		self.items = items.flatMap { StackUser(json: $0) }
+	}
+}
