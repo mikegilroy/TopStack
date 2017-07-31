ef6b894069038245b7c5a9dbf7da5dc7bc1550a6
Mike Gilroy <mjgilroy@me.com>
2017-07-31 15:02:31 +0100

    Implement StackUser

--- TopStack/StackUser.swift
+++ TopStack/StackUser.swift
@@ -8,6 +8,31 @@
 
 import Foundation
 
-protocol User {
+protocol User: JSONInitialisable {
+	var displayName: String { get }
+	var reputation: Int { get }
+	var profileImageURL: String { get }
+}
+
+class StackUser: User {
 	
+	private let kDisplayName = "display_name"
+	private let kReputation = "reputation"
+	private let kProfileImage = "profile_image"
+	
+	let displayName: String
+	let reputation: Int
+	let profileImageURL: String
+	
+	required init?(json: [String : Any]) {
+		guard let displayName = json[kDisplayName] as? String,
+			let reputation = json[kReputation] as? Int,
+			let profileImageURL = json[kProfileImage] as? String else {
+				return nil
+		}
+		
+		self.displayName = displayName
+		self.reputation = reputation
+		self.profileImageURL = profileImageURL
+	}
 }
